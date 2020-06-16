4db1730a3efb97cf07d39eb6222652fdeb67db4b
Roman Piatyntsev <r.piatyntsev@gmail.com>
2020-06-16 17:33:09 +0300

    add ship & bullets

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings():
+    def __init__(self):
+        # main settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
+        # ship settings
+        self.ship_speed = 4
+
+        #bullet settings
+        self.bullet_speed = 10
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 0, 0)
+        self.bullets_allowed = 5
---
+++ ship.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+class Ship():
+    """SHIP"""
+
+    def __init__(self, ai_game, speed=1):
+        # get main screen
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        
+        # get ship screen
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # start position
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.moving_right = False
+        self.moving_left = False
+
+        self.settings = ai_game.settings
+        self.x = float(self.rect.x)
+
+    def update(self):
+        """"Uptates ship position"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Render Model"""
+        self.screen.blit(self.image, self.rect)
