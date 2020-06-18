01c2473fe59a477fdeecad08a400b1f6eb66919b
Roman Piatyntsev <r.piatyntsev@gmail.com>
2020-06-18 19:04:20 +0300

    create alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,23 +4,28 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
-    """AlienInvasion Game"""
+    """AlienInvasion Game Controller"""
 
     def __init__(self):
         pygame.init()
-        self.settings = Settings()
- 
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.screen = pygame.display.set_mode((1200, 800))
+
+        self.settings = Settings()
+        pygame.display.set_caption(self.settings.caption)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption('Alien Invasion')
 
         self.ship = Ship(self)
 
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def _check_events(self):
         for event in pygame.event.get():
@@ -49,14 +54,28 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
-    def _updeate_screen(self):
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
-        for bullet in self.bullets.sprites():
-            bullet.draw_bullet()
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - alien_width
+        available_space_y = self.settings.screen_height - \
+            (alien_height * 3) - self.ship.rect.height
+        number_aliens_x = available_space_x // (2 * alien_width)
+        number_rows = available_space_y // (2 * alien_height)
 
-        pygame.display.flip()
-    
+        # create row of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """ Create alien and add his to row """
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.rect.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.y = alien_height + 2 * alien_height * row_number
+        self.aliens.add(alien)
+
     def _fire_bullet(self):
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
@@ -69,13 +88,29 @@ class AlienInvasion:
             if bullet.rect.y <= (0 - self.settings.bullet_height):
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        self.aliens.update()
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        self.aliens.draw(self.screen)
+        pygame.display.flip()
+
     def run_game(self):
         """RUN GAME"""
         while True:
             self._check_events()
             self.ship.update()
             self._update_bullets()
-            self._updeate_screen()
+            self._update_aliens()
+            self._update_screen()
+            # pygame.time.delay(20)
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
 class Settings():
+    """ Game Settings """
+
     def __init__(self):
         # main settings
+        self.caption = 'Aliens Invasion'
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        
+
         # ship settings
         self.ship_speed = 4
 
@@ -14,3 +17,11 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = (0, 0, 0)
         self.bullets_allowed = 5
+
+        #alien's settings
+        self.alien_speed = 1.0
+        self.alien_drop_speed = 1.0
+        
+        # fleet direction right = 1, direction left = -1
+        self.fleet_direction = 1
+
